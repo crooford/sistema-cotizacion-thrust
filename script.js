91ed40230dfa122bd529f54f4a599e6ca01c65dd
Carlos Escudero <escuderocarlos23@gmail.com>
2023-06-02 14:20:29 -0500

    Changes to be committed: 	deleted:    cotizacion.html 	deleted:    cotizacion.js 	modified:   index.html 	modified:   recursos/css/cotizacion.css 	modified:   recursos/css/main.css 	modified:   script.js

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const servicesSelect = document.getElementById('services');
 const addServiceBtn = document.getElementById('add-service-btn');
 const servicesList = document.getElementById('services-list');
 const generateQuoteBtn = document.getElementById('generate-quote-btn');
-
+const containerDiv = document.getElementById('container')
+const addInfo = document.getElementById('add-info-service');
+const descargaBtn = document.getElementById('btn__descargar');
 let availableServices = []; // Array para almacenar los servicios disponibles
 let selectedServices = []; // Array para almacenar los servicios seleccionados
 
@@ -27,6 +29,9 @@ fetch('informacion.json')
       servicesSelect.appendChild(option);
     });
   });
+
+
+
 
 // Evento al hacer clic en el botón "Agregar"
 addServiceBtn.addEventListener('click', () => {
@@ -92,9 +97,138 @@ addServiceBtn.addEventListener('click', () => {
 // Evento al hacer clic en el botón "Generar cotización"
 generateQuoteBtn.addEventListener('click', () => {
   if (selectedServices.length > 0) {
-    const quoteData = encodeURIComponent(JSON.stringify(selectedServices));
-    window.location.href = `cotizacion.html?quote=${quoteData}`;
+    // Obtener referencia al elemento de la tabla
+    const clienteInfo = document.getElementById('cliente').value;
+    document.getElementById('cliente-recive').value = clienteInfo;
+    const vigenciaInfo= document.getElementById('fechav').value;
+    document.getElementById('fechav-recive').value = vigenciaInfo;
+    const ncotizacion = document.getElementById('ncot').value;
+    document.getElementById('ncot-recive').value = ncotizacion;
+    const fechaInfo = document.getElementById('fecha').value;
+    document.getElementById('fecha-recive').value = fechaInfo;
+    const quoteTable = document.getElementById('quote-table');
+    containerDiv.classList.add('ocult');
+    const sectionCost = document.getElementById('section-cot');
+    sectionCost.classList.remove('ocult');
+    const containerImg = document.getElementById('img-container');
+    containerImg.classList.add('ocult');
+    const containerDescarga = document.getElementById('container-btn-descarga');
+    containerDescarga.classList.remove('ocult');
+
+
+    // Generar la tabla con los servicios seleccionados
+    const selectedItem = document.createElement('div');
+    selectedItem.classList.add('selected-item-group');
+
+    selectedServices.forEach(service => {
+    // primera linea de la cotizacion
+      const serviceSelect = document.createElement('div');
+      serviceSelect.classList.add('service-select');
+
+      // elementos agregados en la primera linea
+      const serviceTitle = document.createElement('h3');
+      serviceTitle.classList.add('service-title');
+      serviceTitle.textContent = service.servicio;
+      serviceSelect.appendChild(serviceTitle);
+
+      const servicePrice = document.createElement('h3');
+      servicePrice.classList.add('service-price');
+      servicePrice.textContent = `$${service.precio} MXN`;
+      serviceSelect.appendChild(servicePrice);
+
+      selectedItem.appendChild(serviceSelect);
+
+
+      const serviceDetails = document.createElement('div');
+      serviceDetails.classList.add('service-details');
+
+      const detailTitle = document.createElement('p');
+      detailTitle.classList.add('detail-title');
+      detailTitle.textContent = 'Detalles:';
+      serviceDetails.appendChild(detailTitle);
+
+      const timeItem = document.createElement('h4');
+      timeItem.classList.add('time-item');
+      timeItem.textContent = `${service.tiempo}`;
+      serviceDetails.appendChild(timeItem);
+
+      selectedItem.appendChild(serviceDetails);
+
+      const detallesList = document.createElement('ul');
+      detallesList.classList.add('list-details');
+      service.detalles.forEach(detalle => {
+          const detalleItem = document.createElement('li');
+          detalleItem.textContent = detalle;
+          detallesList.appendChild(detalleItem);
+        });
+      selectedItem.appendChild(detallesList);
+
+      const serviceEntregable = document.createElement('div');
+      serviceEntregable.classList.add('service-entregable');
+
+      const entregableTitle = document.createElement('p');
+      entregableTitle.classList.add('entregable-title');
+      entregableTitle.textContent = 'Entregable: ';
+      serviceEntregable.appendChild(entregableTitle);
+
+      selectedItem.appendChild(serviceEntregable)
+
+      const entregableList = document.createElement('ul');
+      entregableList.classList.add('list-entregable');
+      service.entregable.forEach(entregable => {
+        const entregableItem = document.createElement('li');
+        entregableItem.textContent = entregable;
+        entregableList.appendChild(entregableItem);
+      });
+      
+
+      // primera linea de la cotizacion
+      const serviceTotalSelect = document.createElement('div');
+      serviceTotalSelect.classList.add('service-total-select');
+      
+      // elementos agregados en la primera linea
+      const serviceTotalTitle = document.createElement('h3');
+      serviceTotalTitle.classList.add('service-total-title');
+      serviceTotalTitle.textContent = 'Total';
+      serviceTotalSelect.appendChild(serviceTotalTitle);
+      
+      selectedItem.appendChild(entregableList);
+    });
+
+    quoteTable.appendChild(selectedItem);
+    
   } else {
     alert('No se han seleccionado servicios. Por favor, agregue al menos uno.');
   }
 });
+
+document.addEventListener("DOMContentLoaded",() =>{
+  descargaBtn.addEventListener("click",() =>{
+    const element = document.section;
+    html2pdf().set({
+      margin: 1,
+      filename: 'cotizacion.pdf',
+      image:{
+        type: 'jpeg',
+        quality: 0.98
+      },
+      html2canvas:{
+        scale: 3,
+        letterRendering: true,
+      },
+      jsPDF:{
+        unit: "in",
+        format: "a4",
+        orientation: 'portrait'
+      }
+      
+    }).from(element).save().catch(err => console.log(err))
+  });
+
+
+
+
+
+});
+
+
